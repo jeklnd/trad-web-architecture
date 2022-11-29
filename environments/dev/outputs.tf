be51db5d500a187bca2c695a7f3b091a0f1dd53a
jeklnd <jdkrensel@gmail.com>
2022-11-29 15:17:38 -0500

    add outputs

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -1,11 +1,11 @@
 output "private_subnets" {
   description = "List of IDs of private subnets"
-  value       = module.vpc.private_subnets
+  value = module.vpc.private_subnets
 }
 
 output "public_subnets" {
   description = "List of IDs of public subnets"
-  value       = module.vpc.public_subnets
+  value = module.vpc.public_subnets
 }
 
 output "vpc-id" {
@@ -16,11 +16,11 @@ output "vpc-id" {
 output "alb_dns_name" {
   value = module.ec2.alb-dns-name
 }
-
+/*
 output "hosted_zone_name_servers" {
   value = module.network.name-servers
 }
-
+*/
 output "cloudfront_domain_name" {
   value = module.network.cloudfront-domain-name
 }
@@ -28,4 +28,27 @@ output "cloudfront_domain_name" {
 output "vpc-cidr" {
   description = "The CIDR range of the VPC"
   value = module.vpc.vpc_cidr_block
+}
+
+output "domain-name" {
+  description = "The name of the domain that you route traffic for"
+  value = module.network.domain-name
+}
+
+data "aws_instances" "bastion-host-public-ipv4-address" {
+  filter {
+    name = "instance.group-id"
+    values = [module.network.bastion-sg]
+  }
+}
+output "bastion-host-public-ipv4-address" {
+  value = data.aws_instances.bastion-host-public-ipv4-address.public_ips
+}
+
+output "ssh-command" {
+  value = "ssh -i /path/key-pair-name.pem instance-user-name@instance-public-dns-name"
+}
+
+output "linux-command-to-get-your-public-ip" {
+  value = "dig +short myip.opendns.com @resolver1.opendns.com"
 }
