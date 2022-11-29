bda4d30f0d841a4e52774c3a0ef73e1a406c37fb
jeklnd <jdkrensel@gmail.com>
2022-11-29 11:56:49 -0500

    add output variable

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,7 +1,11 @@
-output "target-group-arns" {
+output "web-tier-target-group-arns" {
     value = aws_lb_target_group.web-tier.arn
 }
 
 output "alb-dns-name" {
     value = aws_lb.web-tier.dns_name
+}
+
+output "app-tier-target-group-arns" {
+    value = aws_lb_target_group.app-tier.arn
 }
