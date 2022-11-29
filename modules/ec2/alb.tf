44bcfb5c879020fb42eab0fbeddbfe445cbad827
jeklnd <jdkrensel@gmail.com>
2022-11-29 11:44:13 -0500

    add app-tier alb listener and target group

--- modules/ec2/alb.tf
+++ modules/ec2/alb.tf
@@ -52,3 +52,34 @@ resource aws_lb "app-tier" {
     security_groups = var.app-tier-alb-sg
     subnets = var.private-app-subnets
 }
+
+resource "aws_lb_listener" "app-tier" {
+    port = 80
+    protocol = "HTTP"
+    load_balancer_arn = aws_lb.app-tier.arn
+
+    default_action {
+        type = "forward"
+        target_group_arn = aws_lb_target_group.app-tier.arn
+    }
+}
+
+resource "aws_lb_target_group" "app-tier" {
+    name_prefix = "app-"
+    target_type = "instance"
+    port = 80
+    protocol = "HTTP"
+    vpc_id = var.vpc-id
+
+    health_check {
+        enabled = true
+        healthy_threshold = 2
+        interval = 15
+        matcher = "200-299"
+        path = "/"
+        port = 80
+        protocol = "HTTP"
+        timeout = "14"
+        unhealthy_threshold = 3
+    }
+}
