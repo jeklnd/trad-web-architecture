01f796a00b879a03faf4e7c9a862c329101b15ae
jeklnd <jdkrensel@gmail.com>
2022-11-29 10:24:23 -0500

    update comment headings, add sg for app-tier alb

--- modules/network/security.tf
+++ modules/network/security.tf
@@ -1,10 +1,11 @@
-################################################################################
-# Web security group
-################################################################################
+####################################
+# web tier security groups
+####################################
 
+# web servers
 resource "aws_security_group" "web" {
     name = "${var.env}-web-sg"
-    description = "web server security group"
+    description = "web tier servers security group"
     vpc_id = var.vpc-id
 }
 
@@ -35,9 +36,7 @@ resource "aws_security_group_rule" "outbound_all" {
     security_group_id = aws_security_group.web.id
 }
 
-################################################################################
-# ALB security group
-################################################################################
+# alb
 resource "aws_security_group" "alb" {
     name = "${var.env}-alb-sg"
     description = "alb security group"
@@ -55,11 +54,48 @@ resource "aws_security_group_rule" "inbound_all" {
 }
 
 resource "aws_security_group_rule" "web-listener-and-health-check" {
-    description = "Allow outbound traffic to instances on the instance listener and health check port."
+    description = "Allow outbound traffic to instances on the instance listener and health check ports."
     type = "egress"
     from_port = 80
     to_port = 80
     source_security_group_id = aws_security_group.web.id
     protocol = "tcp"
     security_group_id = aws_security_group.alb.id
+}
+
+####################################
+# app tier security groups
+####################################
+/*
+# servers
+resource "aws_security_group" "app-tier-servers" {
+    name = "${var.env}-app-tier-servers-sg"
+    description = "app tier servers security group"
+    vpc_id = var.vpc.id
+}
+*/
+
+resource "aws_security_group" "app-tier-alb" {
+    name = "${var.env}-app-tier-alb-sg"
+    description = "app-tier alb security group"
+    vpc_id = var.vpc-id
+}
+
+resource "aws_security_group_rule" "app-tier-alb-ingress" {
+    type = "ingress"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = [var.vpc-cidr]
+    security_group_id = aws_security_group.app-tier-alb.id
+    description = "allow  inbound traffic from the VPC CIDR on the load balancer listener port"
+}
+
+resource "aws_security_group_rule" "app-tier-alb-egress" {
+    type = "egress"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    security_group_id = aws_security_group.app-tier-alb.id
+    description = "allow outbound traffic to the instances on the instance listener and health check ports"
 }
