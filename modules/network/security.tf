720ef4410e776245782eee78fbc42fe48a312a98
jeklnd <jdkrensel@gmail.com>
2022-11-29 17:28:13 -0500

    small fix

--- modules/network/security.tf
+++ modules/network/security.tf
@@ -21,6 +21,7 @@ resource "aws_security_group_rule" "egress-to-vpc" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
+    #cidr_blocks = ["0.0.0.0/0"]
     cidr_blocks = [var.vpc-cidr]
     security_group_id = aws_security_group.public.id
 }
