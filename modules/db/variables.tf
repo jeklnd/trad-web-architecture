0bde7b94cdd11552c6bcd93d8c3b715a800cb78e
jeklnd <jdkrensel@gmail.com>
2022-11-29 18:46:01 -0500

    added 4 variables

---
+++ modules/db/variables.tf
@@ -0,0 +1,15 @@
+variable "db_username" {
+    default = ""
+}
+
+variable "db_password" {
+    default = ""
+}
+
+variable "security_group_ids" {
+    default = []
+}
+
+variable "subnet_ids" {
+    default = []
+}
