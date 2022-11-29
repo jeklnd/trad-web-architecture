9f11cc29fc9d37c2bebcf0cce9139d4ac5cb97b9
jeklnd <jdkrensel@gmail.com>
2022-11-29 11:56:39 -0500

    add app-tier asg, add comment headers

--- modules/ec2/asg.tf
+++ modules/ec2/asg.tf
@@ -1,18 +1,22 @@
+####################################
+# web tier asg
+####################################
+
 resource "aws_autoscaling_group" "web" {
     name = "web-servers"
     min_size = 2
     desired_capacity = 2
     max_size = 6
-    vpc_zone_identifier = var.vpc-zone-identifier
+    vpc_zone_identifier = var.web-tier-vpc-zone-identifier
     launch_configuration = aws_launch_configuration.web.name
     health_check_type = "ELB"
-    target_group_arns = [var.target-group-arns]
+    target_group_arns = [var.web-tier-target-group-arns]
     wait_for_capacity_timeout = "10m"
 }
 
 resource "aws_launch_configuration" "web" {
     name_prefix = "web-servers-"
-    image_id = "ami-0b0dcb5067f052a63" # Amazon Linux 2 Kernel 5.10 AMI 2.0.20221103.3 x86_64 HVM gp2
+    image_id = var.image-id # Amazon Linux 2 Kernel 5.10 AMI 2.0.20221103.3 x86_64 HVM gp2
     instance_type = var.instance-type
     key_name = var.key-name
     security_groups = var.web-servers-sg
@@ -29,6 +33,33 @@ EOF
 #<p>DB address: ${db_address}</p>
 #<p>DB port: ${db_port}</p>
 
+    
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+
+####################################
+# app tier asg
+####################################
+resource "aws_autoscaling_group" "app-tier" {
+    name = "app-tier-servers"
+    min_size = 2
+    desired_capacity = 2
+    max_size = 6
+    vpc_zone_identifier = var.app-tier-vpc-zone-identifier
+    launch_configuration = aws_launch_configuration.app.name
+    health_check_type = "ELB"
+    target_group_arns = [var.app-tier-target-group-arns]
+    wait_for_capacity_timeout = "10m"
+}
+
+resource "aws_launch_configuration" "app" {
+    name_prefix = "app-servers-"
+    image_id = var.image-id
+    instance_type = var.instance-type
+    key_name = var.key-name
+    security_groups = var.app-tier-servers-sg
     
     lifecycle {
         create_before_destroy = true
